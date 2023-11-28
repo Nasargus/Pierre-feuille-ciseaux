cf6ef5d3cacdfa7afc663f003a27b499fcb95e4c
Nasargus <nassima.yahiaoui.geol@gmail.com>
2023-11-28 13:52:31 +0100

    first commit

---
+++ Page1/javascript1/Script1/script1.js
@@ -0,0 +1,9 @@
+// Récupérer le bouton par son ID
+var bouton = document.getElementById('btn');
+
+
+// Ajouter un gestionnaire d'événement click au bouton
+bouton.addEventListener('click', function() {
+    // Redirection vers l'URL souhaitée
+    window.location.href ="../Page2/index.html";
+});
